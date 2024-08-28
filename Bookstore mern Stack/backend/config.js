22f51ded517c761459d2b31cfe2bcfd0f0fee15d
Siddharth Dayal <130763953+Sidlion03@users.noreply.github.com>
2024-08-28 19:44:08 +0530

    Bookstore FullStack
    Both frontend and backend

---
+++ Bookstore mern Stack/backend/config.js
@@ -0,0 +1,7 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  'mongodb+srv://root:root@book-store-mern.4m8i3bt.mongodb.net/book-store?retryWrites=true&w=majority&appName=Book-store-MERN';
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
